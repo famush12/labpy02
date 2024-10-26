4f6b7b8fdbcc8d99c9a3de940d7bc255f63a4b57
fawaidil mustofa <muesthafaelfatah@gmail.com>
2024-10-26 12:39:23 +0700

    main.1

---
+++ main (1).py
@@ -0,0 +1,7 @@
+#pemesanan tiket bioskop
+num = input("50: ")
+if (int(num) >= 100):
+    print(num, "adalah bilangan positif")
+else:
+    print(num, "adalah blangan negatif")
+print("ini di luar pernyataan if")    
